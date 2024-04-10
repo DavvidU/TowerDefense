288d255e54b9efe49a34e13384ab748a38d7d361
CinekTV <cinekv15@gmail.com>
2024-04-10 10:53:23 +0200

    glosnosc

--- Scripts/PlayMusic.cs
+++ Scripts/PlayMusic.cs
@@ -1,13 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayMusic : MonoBehaviour
 {
+    [SerializeField] Slider volumeSlider;
     // Start is called before the first frame update
     AudioSource aud;
     void Start()
     {
+        if(!PlayerPrefs.HasKey("musicVolume")){
+            PlayerPrefs.SetFloat("musicVolume", 1);
+            Load();
+        }
         aud = GetComponent<AudioSource>();
         aud.Play();
 
@@ -17,5 +23,16 @@ public class PlayMusic : MonoBehaviour
     void Update()
     {
         
+    }
+    public void ChangeVolume(){
+        AudioListener.volume = volumeSlider.value;
+        Save();
+    }
+    private void Load(){
+        volumeSlider.value = PlayerPrefs.GetFloat("musicVolume");
+    }
+
+    private void Save(){
+        PlayerPrefs.SetFloat("musicvolume", volumeSlider.value);
     }
 }
