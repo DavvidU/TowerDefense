b797891768653dff7bbe74de844ba7d68c866025
Artur Leszczak <al.ok1@wp.pl>
2024-06-18 01:07:51 +0200

    Naprawiono prefab - dodano collidery, dokumentacja kodu plików Strzala.cs i SimplePushing.cs

--- Scripts/Traps/PushingTraps/SimplePushing.cs
+++ Scripts/Traps/PushingTraps/SimplePushing.cs
@@ -4,47 +4,77 @@ using System.Threading;
 using UnityEditor;
 using UnityEngine;
 
+/**
+*The `SimplePushing` class is responsible for shooting arrows at a defined speed and limit,
+* with functionality to create and shoot arrows in a game environment, you should add this class
+* to GameObject .
+* @author Artur Leszczak
+* @version 1.0.0
+*/
 public class SimplePushing : MonoBehaviour
 {
     [SerializeField]
-    GameObject StrzalaPrefab;
+    GameObject StrzalaPrefab; //prefab of arrow
 
-    
+    //Defines a limit of arrows
     public int ArrowLimit = 50;
     private int ArrowCounter = 0;
-    public Transform spawnPoint;
+    public Transform spawnPoint; //point of arrow spawn 
     public float shootingSpeed;
     private float timer;
     // Start is called before the first frame update
     void Start()
     {
-
-        
-
-        shootingSpeed = 1f; //jedna sekunda
+        shootingSpeed = 1f; //one second
         timer = 0f;
     }
-    void Update()
-    {
+    
+    /**
+    * This method is called once per frame. It checks if it's time to shoot an arrow and does so if conditions are met.
+    *
+    * @author Artur Leszczak
+    * @version 1.0.0
+    */
+    void Update() {
+        // Increment the timer by the time passed since the last frame
         timer += Time.deltaTime;
-        if(timer >= shootingSpeed && ArrowCounter<=ArrowLimit)
-        {
+
+        // Check if the timer has reached or exceeded the shooting speed and if the arrow limit has not been reached
+        if(timer >= shootingSpeed && ArrowCounter <= ArrowLimit) {
+            // Increment the arrow counter
             ArrowCounter++;
+
+            // Call the shoot method to create and shoot an arrow
             shoot();
+
+            // Reset the timer
             timer = 0f;
         }
-       
     }
-    void shoot()
-    {
+
+    /**
+    * This method is responsible for creating and shooting an arrow.
+    *
+    * @return void
+    * @author Artur Leszczak
+    * @version 1.0.0
+    */
+    void shoot() {
+        // Instantiate a new arrow GameObject at the spawn point's position and rotation
         GameObject arrow = Instantiate(StrzalaPrefab, spawnPoint.position, spawnPoint.rotation);
 
-        // Ustawienie kierunku strza³y zgodnie z kierunkiem pu³apki
+        // Get the Strzala component from the instantiated arrow GameObject
         Strzala arrowScript = arrow.GetComponent<Strzala>();
-        if (arrowScript != null)
-        {
+
+        // Check if the Strzala component exists
+        if (arrowScript != null) {
+            // Set the arrow's direction opposite to the spawn point's right direction
             arrowScript.UstawKierunek(-spawnPoint.right);
+
+            // Set the arrow's speed to 8 units per second
             arrowScript.setSpeed(8f); 
+
+            // Set the arrow's range to 5 units
             arrowScript.setRange(5f); 
         }
     }
--- Scripts/Traps/PushingTraps/Strzala.cs
+++ Scripts/Traps/PushingTraps/Strzala.cs
@@ -4,18 +4,24 @@ using System.Threading;
 using UnityEngine;
 using UnityEngine.UIElements;
 
+/**
+ * Klasa definiuj¹ca obiekt bêd¹cy strza³¹ wystrzeliwany z dispensera
+ * 
+ * @author Artur Leszczak
+ * @version 1.0.0
+ */
 public class Strzala : MonoBehaviour
 {
-    private float speed; 
-    private float range; 
-    private float time;
-    private float przyspieszenie;
-    private float predkoscOpadania;
+    private float speed; //arrow speed (default 16f)
+    private float range; //arrow range (default 5f)
+    private float time; 
+    private float przyspieszenie; //arrow velocity (default 0.001f)
+    private float predkoscOpadania; //arrow gravity speed (default 0)
     private GameObject thisArrow;
-    private float damage;
+    private float damage; //defines how much damage arrow give (default 15f)
     private Rigidbody rb;
     private float timer;
-    private float timeToDestroy;
+    private float timeToDestroy; //time to destroy the arrow in seconds (default 3)
     private Vector3 kierunek;
 
     // Start is called before the first frame update
@@ -34,25 +40,36 @@ public class Strzala : MonoBehaviour
         kierunek = new Vector3(1f,0.2f,0f);
     }
 
+    /*Defines speed of arrow*/
     public void setSpeed(float speed)
     {
         this.speed = speed;
     }
 
+    /*Defines how far arrow fligt*/
     public void setRange(float range)
     {
         this.range = range;
     }
 
+    /*Returns this arrow*/
     public GameObject getObject()
     {
         return this.thisArrow;
     }
 
+    /*Returns damage set for this entity*/
     public float getDamage()
     {
         return this.damage;
     }
+
+    /**
+     * This metod sets a direction of arrow flight.
+     * @param Vector3 kierunek - takes a Vector3 variable to defines a position to flight.
+     * @author Artur Leszczak
+     * @version 1.0.0
+     */
     public void UstawKierunek(Vector3 kierunek)
     {
         this.kierunek = kierunek.normalized;
